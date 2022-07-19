1a0ec7a8ffdef289e1de4067e672263ed4f7d03a
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2022-07-19 17:29:04 +0200

    Stream through BAM file with progress display

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Spike variants into BAM files (without all the hassle)
 ## Running
 
 ```
-$ cargo run -- examples/nsd1-grch38.yaml \
+$ cargo run -- \
+    examples/nsd1-grch38.yaml \
+    ../hs38DH.fa \
     ../NA12864.chr5.bam \
     ../NA12864.chr5.mut.bam
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #![allow(unused)]
 
+use std::str;
 use std::{fs::File, io::Read};
 
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
+use rust_htslib::{
+    bam,
+    bam::{HeaderView, Read as BamRead},
+};
 use serde::{Deserialize, Serialize};
 
 /// Specify spike-in of a small variant
@@ -56,6 +62,8 @@ struct Instructions {
 /// Overall configuration of espike
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Config {
+    /// Path to the FAI indexed FASTA reference file
+    path_reference: std::path::PathBuf,
     /// Path to the input BAM file
     path_bam_in: std::path::PathBuf,
     /// Path to the output BAM file
@@ -70,6 +78,9 @@ struct Cli {
     /// The path to the instructions YAML file
     #[clap(parse(from_os_str))]
     path_instructions: std::path::PathBuf,
+    /// The path to the FAI-indexed reference file
+    #[clap(parse(from_os_str))]
+    path_reference: std::path::PathBuf,
     /// The path to the input BAM file
     #[clap(parse(from_os_str))]
     path_bam_in: std::path::PathBuf,
@@ -88,17 +99,65 @@ fn load_instructions(file: &std::path::PathBuf) -> Instructions {
 
     let result: Instructions =
         serde_yaml::from_str(&contents).expect("Unable to parse file contents");
-    return result;
+    result
+}
+
+fn run(config: &Config) {
+    // Initialize BAM reader and writer
+    let mut bam_in = bam::IndexedReader::from_path(config.path_bam_in.clone()).unwrap();
+    bam_in.set_threads(2);
+    let header = bam::Header::from_template(bam_in.header());
+    let mut bam_out =
+        bam::Writer::from_path(config.path_bam_out.clone(), &header, bam::Format::Bam).unwrap();
+    bam_out.set_threads(2);
+
+    let header_view = HeaderView::from_header(&header);
+    for tid in (0..(header_view.target_count() - 1)) {
+        if (tid != 4) {
+            continue; // TODO: for testing only
+        }
+        let target_name = header_view.tid2name(tid);
+        let target_len = header_view.target_len(tid).expect("Invalid target");
+        bam_in.fetch(tid).expect("Could not fetch contig");
+
+        println!(
+            "Processing contig {:?} ({})",
+            str::from_utf8(&target_name).unwrap(),
+            tid
+        );
+
+        let bar = ProgressBar::new(target_len);
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template(
+                    "[{elapsed_precise}/{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+                )
+                .progress_chars("##-"),
+        );
+
+        // Iterate over input file, write to output file
+        for r in bam_in.records() {
+            let record = r.unwrap();
+            let record_pos = record.pos();
+            bar.set_position(record_pos.try_into().unwrap());
+            if record.is_reverse() {
+                bam_out.write(&record).unwrap();
+            }
+        }
+        bar.set_position(target_len);
+    }
 }
 
 fn main() {
     let args = Cli::parse();
     let config: Config = Config {
+        path_reference: args.path_reference,
         path_bam_in: args.path_bam_in,
         path_bam_out: args.path_bam_out,
         instructions: load_instructions(&args.path_instructions),
     };
-    println!("Running 'espik' - your BAM spiker.");
+    println!("Running 'espik' - your friendly BAM spiker.");
     println!("Configuration is {:?}", config);
+    run(&config);
     println!("All done. Have a nice day!");
 }
